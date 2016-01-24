e8b9720edff63d318b323206ab9e5be8d6556c63
salomoneb <salomone.l.baquis@gmail.com>
2016-01-24 14:56:37 -0500

    cleaned up pseudocode; added string trim and field reset code

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
-
-$("#submit-btn").click(change);
+	// Code won't execute until DOM is ready.
 $("form").submit(function(event) {
 		event.preventDefault();
+		$("form").children("#city-type").val("");
 	});
+	// Prevents default form submission.
+	// Resets text input field after submission.
 
-function save() {
-	event.preventDefault();
-}
+$("#submit-btn").click(change);
+	// When a user clicks the "Update" button, the background changes.
 
 function change() {
-	var city = $("#city-type").val();
+	var city = $("#city-type").val().trim();
+	// Defines user-inputted city and removes extra spaces before and after string. 
 
 	if (city == "New York" || city == "NYC" || city == "New York City") {
 		$("body").removeClass();
 		$("body").addClass("nyc");
-		}
+	}
+	// Removes existing class.
+	// Adds a new class (with background defined in CSS). 
 	else if (city == "San Francisco" || city == "SF" || city == "Bay Area") {
 		$("body").removeClass();
-		$("body").addClass("sf");
+		$("body").addClass("sf");	
 	}
 	else if (city == "Los Angeles" || city == "LA" || city == "LAX") {
 		$("body").removeClass();
@@ -35,16 +39,6 @@ function change() {
 	else {
 		$("body").removeClass();
 	}
+	// Resets background to default if user enters anything other than defined city.
 }
 });
-
-/* 
-When a user clicks the "Update" button, the background changes.
-To do this: 
-A user enters a city string.
-If there's an existing body class, that class is removed. 
-The body class that corresponds to the specific text input is added, modifying the background. 
-The new background remains until the page is refreshed or another 
-If a user submits a string that isn't specified then the body class is removed and the background image reverts to default.
-*/
-
